a0236e8ab541f8c0e69c2d20535df0275db00ce1
Nebiyou Yemam <proneed@yahoo.com>
2023-10-06 13:32:21 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if comleted succesfully
+ *
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
