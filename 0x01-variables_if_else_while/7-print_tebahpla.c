f30f438e1215cd72841cf2d22398af0ea16f64ae
Nebiyou Yemam <proneed@yahoo.com>
2023-10-07 02:59:58 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
