9f5582d3904a7ce22d5024f576f979625bda76b5
Nebiyou Yemam <proneed@yahoo.com>
2023-10-07 05:59:08 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		if (i <= 56)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(i);
+	return (0);
+}
