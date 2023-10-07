a2cee508caa30cb60680179d7c49e3f1c58bbc8d
Nebiyou Yemam <proneed@yahoo.com>
2023-10-07 02:13:27 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		else
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
