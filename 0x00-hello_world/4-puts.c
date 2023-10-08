38347194ff5eac0130dfcf5ed6dac732a43e8895
Nebiyou Yemam <proneed@yahoo.com>
2023-10-08 12:52:30 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
