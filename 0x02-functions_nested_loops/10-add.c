8ba52e7f158825a28587ee0dfe2c069853914939
Nebiyou Yemam <proneed@yahoo.com>
2023-10-10 05:22:15 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: 0 if completed successfully
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
