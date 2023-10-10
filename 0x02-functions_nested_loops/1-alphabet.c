44221dc03aaa43febe01cba3ccdbf57828549f79
Nebiyou Yemam <proneed@yahoo.com>
2023-10-10 01:05:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if completed successfully
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
