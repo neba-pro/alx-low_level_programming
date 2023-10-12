4d10f6c0266e4e14c749c42e7ddc2ad337e6c00d
Nebiyou Yemam <proneed@yahoo.com>
2023-10-12 03:21:23 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if uppercase
+ * @c: parameter passed from the user
+ * Return: 0 if completed successfully
+ */
+
+int int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
