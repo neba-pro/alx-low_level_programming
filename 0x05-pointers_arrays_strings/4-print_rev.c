93e7f9653bbfa96569165fabf5ad765b9d842adc
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 05:31:05 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
-	for (j = length - 1; j <= 0; j--)
+	for (j = length; j >= 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
