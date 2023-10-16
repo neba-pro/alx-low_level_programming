766c44af8a17c6f9de7edb0ec8b54de02e1fd8cf
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 05:14:01 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints the passed string to stdout
+ * @str: passed string
+ *
+ */
+
+void _puts(char *str)
+{
+	int length, j;
+
+	for (lenth = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (j = 0; j < length; j++)
+		_putchar(s[j]);
+	_putchar('\n');
+}
