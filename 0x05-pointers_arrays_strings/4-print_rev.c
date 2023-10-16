597ecfc0cb1329ed7f2a414533d88341afe73029
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 05:22:44 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the passed string in reverse
+ * @s: string to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int length, j;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (j = length - 1; j <= 0; j--)
+		_putchar(s[j[);
+	_putchar('\n');
+}
