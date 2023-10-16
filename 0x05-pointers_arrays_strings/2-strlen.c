983ff992a235976a9f8ed6429a28de6dfaf384a2
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 05:03:41 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - computes the length of the passed string
+ * @s: string to be computed
+ * Return: 0 if completed successfully
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
