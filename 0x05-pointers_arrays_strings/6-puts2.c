1732f4605fcf4026e673499096c728735cd9b70c
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 21:40:11 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 1;
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
