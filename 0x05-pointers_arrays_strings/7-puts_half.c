816c1c9d62444243e542162644d1c0bba777905e
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 22:04:26 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_hal - prints half of the passed string
+ * puts_half - prints half of the passed string
  * @str: string to be truncated
  *
  * Return: 0 if com0leted successfully
@@ -9,4 +9,22 @@
 
 void puts_half(char *str)
 {
+	int i, j, n;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+		for (j = n; j < i; j++)
+			_putchar(str[j]);
+	}
+	else
+	{
+		n = (i - 1) / 2;
+		for (j = n + 1; j < 1; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
