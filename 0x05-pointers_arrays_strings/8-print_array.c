47fcd9d37a2b0137698826188a1f2281061dcdb0
Nebiyou Yemam <proneed@yahoo.com>
2023-10-16 23:19:48 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,16 +11,22 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n == 0 || n < 0)
 	{
-		if (n != 1)
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			if (i != n - 1)
-				printf("%d, ", a[i]);
+			if (n != 1)
+			{
+				if (i != n - 1)
+					printf("%d, ", a[i]);
+				else
+					printf("%d\n", a[i]);
+			}
 			else
-				printf("%d\n", a[i]);
+				printf("%d\n", a[0]);
 		}
-		else
-			printf("%d\n", a[0]);
 	}
 }
