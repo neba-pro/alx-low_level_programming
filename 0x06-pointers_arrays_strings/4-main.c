20f4603cc509f5134e716b647e90ed9cae91e549
Nebiyou Yemam <proneed@yahoo.com>
2023-10-18 23:28:17 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * print_array - function to print array
  * @a: an array of integers
  * @n: the number of elements to swap
  *
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * reverse_array - reverses array
