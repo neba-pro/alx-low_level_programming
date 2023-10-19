af12233e273ce754eea805a8c43f9893af5344a6
Nebiyou Yemam <proneed@yahoo.com>
2023-10-19 01:56:53 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes strings
+ * @s: string to be capitalized
+ * Return: a pointer to the string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] == 46 || s[i] == 32) && (s[i + 1] >= 97 && s[i + 1] <= 122) && (!(s[i + 1] <= 90)))
+		{
+			s[i + 1] = s[i] + 32;
+		}
+	}
+	return (s);
+}
