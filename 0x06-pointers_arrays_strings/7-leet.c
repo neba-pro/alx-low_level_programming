44d8f29f75d4b1fcf028f517473c57a92d98a26a
Nebiyou Yemam <proneed@yahoo.com>
2023-10-19 05:12:21 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,20 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char num_arr[] = {'4', '3', '0', '7', '1'};
+	char upper_let[] = {'A', 'E', '0', 'T', 'L'};
+	char lower_let[] = {'a', 'e', '0', 't', 'l'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == upper_let[j] || s[i] == lower_let[j])
+			{
+				s[i] = num_arr[j];
+			}
+		}
 	}
 	return (s);
 }
