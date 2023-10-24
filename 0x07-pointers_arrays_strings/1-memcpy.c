d4e35d00ade090aa696265bf8e264e9afbc302c6
Nebiyou Yemam <proneed@yahoo.com>
2023-10-23 21:34:06 -0700

    task 1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be filled in
+ * Return: returns a pointer to a memory location
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	while (j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
