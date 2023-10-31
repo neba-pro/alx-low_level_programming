e56aec3e77dc699920f49cd1bbf3e0afaf57ea97
Nebiyou Yemam <proneed@yahoo.com>
2023-10-31 05:04:19 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	new = malloc((sizeof(char) * size) + 1);
+	new = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		new[i] = c;
 	new[i] = '\0';
