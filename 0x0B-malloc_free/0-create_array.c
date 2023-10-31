8fea7a9e23c0697dd423a2596a96fef566511b6e
Nebiyou Yemam <proneed@yahoo.com>
2023-10-31 04:45:33 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - creates array with size of size
+ * @size: size of array to be created
+ * @c: initializing character
+ * Return: a pointer to the created array;
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *new;
+
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		new = malloc(sizeof(unsigned int) * size);
+		for (i = 0; i < size; i++)
+			new[i] = c;
+		new[i] = '\0';
+	}
+	return (new);
+}
