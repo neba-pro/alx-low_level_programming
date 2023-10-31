2d58f5398a487ddf6b5b180b312331dda617fa3c
Nebiyou Yemam <proneed@yahoo.com>
2023-10-31 06:43:01 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 {
 	char *new;
 	int i, j, k, l;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2= "";
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
