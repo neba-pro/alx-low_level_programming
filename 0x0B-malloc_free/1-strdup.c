e49fdd8d3132e564e1f9112af68930f29cb8b11a
Nebiyou Yemam <proneed@yahoo.com>
2023-10-31 05:51:32 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate the string passed
+ * @str: string to be dupulicated
+ * Return: a pointer to the dupulicated memory location
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(str));
+	for (i = 0; str[i] != '\0'; i++)
+		new[i] = str[i];
+	new[i] = '\0';
+	return (new);
+}
