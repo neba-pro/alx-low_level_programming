1d25cd135a938b7db031ef2721a6f3e77292e155
Nebiyou Yemam Ebrahim <proneed@yahoo.com>
2023-11-02 10:48:42 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,16 +11,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int arr[width][height];
+	int i, j, k, l = 0;
+	int **arr;
 
-	for (i = 0; i < width; i++)
+	if (width <= 0 || height <= 0)
 	{
-		for (j = 0; j < height; j++)
+		return (0);
+	}
+	arr = malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		*(arr + i) = malloc(width * sizeof(int));
+		if (*(arr + i) == 0)
+		{
+			l = 1;
+			break;
+		}
+		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
 		}
-		_putchar('\n');
+	}
+	if (l == 1)
+	{
+		for (k = 0; k <= i; k++)
+			{
+				free(*(arr + k));
+			}
+		free(arr);
 	}
 	return (arr);
 }
